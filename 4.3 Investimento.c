36abb315c065ca94dccf4f4004eef8124b288dd8
Yasmim Lais da Silva Germanio <yasmimls.germanio@gmail.com>
2026-04-05 20:15:27 -0300

    Implement position tracking with a matrix
    Added a matrix to track positions and updated movement logic.

--- 4.3 Investimento.c
+++ 4.3 Investimento.c
@@ -3,28 +3,44 @@
 int main() {
     char movimento;
     int x = 0, y = 0;
+    int matriz[4][4] = {0};
 
-    // Recebenmos 20 carcteres
+    matriz[y][x]++;  // conta a posi��o inicial
+
     for (int i = 0; i < 20; i++) {
-
         scanf(" %c", &movimento);
 
+        int nx = x, ny = y; 
+
         switch (movimento) {
-            case 'c': // Cima:  - (Y)
-                y--;
-                break;
-            case 'b': // Baixo: + (Y)
-                y++;
-                break;
-            case 'e': // Esquerda: - (X)
-                x--;
-                break;
-            case 'd': // Direita: + (X)
-                x++;
-                break;
+            
+            case 'c': ny--; break;  // cima
+            case 'b': ny++; break;  // baixo
+            case 'e': nx--; break;  // esquerda
+            case 'd': nx++; break;  // direita
+        }
+
+        // S� move se n�o sair da matriz
+        if (nx >= 0 && nx < 4 && ny >= 0 && ny < 4) {
+            x = nx;
+            y = ny;
+        }
+
+        matriz[y][x]++;
+    }
+
+    int maxVisitas = -1, melhorCol = 0, melhorLin = 0; // Encontra a c�lula mais visitada
+    
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (matriz[i][j] > maxVisitas) {
+                maxVisitas = matriz[i][j];
+                melhorLin = i;  // linha
+                melhorCol = j;  // coluna
+            }
         }
     }
 
-    printf("Coordenada X:%d, Y:%d\n", x, y);
+    printf("Coordenada X:%d, Y:%d\n", melhorCol, melhorLin);
     return 0;
 }
